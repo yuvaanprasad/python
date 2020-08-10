75d6db0c4cdad362e85047f94a90138a0acca73f
Katacoda Scenario <scenario@katacoda.com>
2020-08-10 16:20:10 +0000

    added booleans.py and completed lesson on booleans

---
+++ booleans.py
@@ -0,0 +1,28 @@
+"""Booleans are variables where value is either True or 
+False """
+# Example
+
+boolean_one = True
+boolean_two = False
+
+print(boolean_one)
+print(boolean_two)
+
+""" BOOLEAN OPERATORS
+    Operator    Description
+-------------------------------
+    ==          Equal to
+    >           Greater than
+    >=          Greater than or equal
+    <           Less than
+    <=          Less than or equal
+    !=          Not equal
+"""
+""" Boolean Operators
+    Operator    Description
+    and         Evaluates to True/False if both are True/False
+    or          Evaluates to True/False if either is True/False
+    not         Evaluates to opposite
+"""
+
+
