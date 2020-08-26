f300d77e071bb9338f10b7e244bcbb70a59d8533
Katacoda Scenario <scenario@katacoda.com>
2020-08-26 13:55:03 +0000

    completed lesson on conditionals and added conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,21 @@
+""" Conditionals are the if/else statement basically. It
+evaluates to a boolean """
+
+""" By convention, code blocks are indented by 4 
+spaces """
+
+# Example
+print('Example:')
+
+age = 18
+
+if age < 13:
+    print('You are a child.')
+elif age >= 13 and age < 18:
+    print('You are a teenager')
+else:
+    print('You are an adult')
+
+
+
+
