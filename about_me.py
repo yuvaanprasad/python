8189299a39b5b3eb7e93f5f8dae0ec259e7b5b95
Katacoda Scenario <scenario@katacoda.com>
2020-11-25 07:37:07 +0000

    Completed Project 1: about_me.py

---
+++ about_me.py
@@ -0,0 +1,22 @@
+print("Hi, I can code in Python, using O'Reilly SafariBooks and GitHub!")
+
+print("My favourite animals are rabbits!")
+print("U#####")
+print(" ||||")
+print('''I live in Essex
+
+       __
+ ______||______
+ |            |
+ | ##         |
+ | ##         |
+ |       ##   |
+ |       ##   |
+_|____________|_
+''')
+
+year = input('What year were you born? ')
+year = int(year)
+age = 2025 - year
+print('In 2025, you will be...', age,)
+print("Your age in dog years will be...", age*7,)
